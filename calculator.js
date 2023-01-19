e0e90907155549c3e7a3fe5938f79e3bfa6e8e89
Johnny Bieniek <jjbieniek@gmail.com>
2023-01-19 12:19:15 -0500

    finished a very simple calculator that adds two numbers together

--- calculator.js
+++ calculator.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (request, response) => {
-  response.send("Hello World");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  var num1 = Number(req.body.number1);
+  var num2 = Number(req.body.number2);
+
+  var result = num1 + num2;
+
+  res.send("The result is " + result);
 });
 
 app.listen(3000, () => {
